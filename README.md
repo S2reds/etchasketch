9b1e07a4f21fb9a55602c6fefd0b3ba99f724735
Kyle <thevarsin@gmail.com>
2023-02-02 15:34:44 -0800

    Added new hovered class and clear button

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# etchasketch
+# project etchasketch
+This is a project from The Odin Project curriculum.
+
+Outcome:
+    - Become more familiar with manipulating DOM elements.
--- scripts.js
+++ scripts.js
@@ -5,3 +5,35 @@ for (let i = 0; i < 256; i++) {
     square.classList.add('square')
     document.querySelector('#container').appendChild(square)
 }
+
+const changeGrid = document.querySelector('button')
+changeGrid.addEventListener('click', () => {
+    let gridsize = +prompt('How many squares per side?')
+    if (gridsize > 100) +prompt('Pick a number under 100')
+    gridsize2 = gridsize * gridsize
+    let containersize = gridsize * 16
+    squareBox.style.width = `${containersize}px`
+    squareBox.style.height = `${containersize}px`
+    squareBox.innerHTML = '';
+    for (let i = 0; i < gridsize2; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        document.querySelector('#container').appendChild(square);
+        }
+    });
+
+
+    
+    squareBox.addEventListener('mouseover', function(e) {
+        if (e.target.matches('.square')) {
+            e.target.classList.add('hovered')
+        }
+    });
+    
+    
+    const clear = document.querySelector('#clear')
+clear.addEventListener('click', () => {
+    const box = document.querySelectorAll('.square')
+    box.forEach(b => b.classList.remove('hovered'))
+})
+
